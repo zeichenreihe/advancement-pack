247e479c4d2cc9d04551274ef8fc3809592a1f4e
Johannes Schmatz <johannes_schmatz@gmx.de>
2021-12-12 23:09:44 +0100

    now prints a working list of touples with filenames and mojang json

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 
 class Mcid():
     # minecraft:name
@@ -35,15 +36,6 @@ class Mcid():
 
         output = output + self.name + fileending
         return output
-
-with open("config.yaml") as file:
-    try:
-        data = yaml.safe_load(file)
-        file.close()
-    except:
-        print("yaml.safe_load crashed!!!!")
-        file.close()
-        exit(1)
 
 criteria_requirements = '''
 	"criteria": {
@@ -97,19 +89,26 @@ default_out = '''{brackets_open}
     {criteria_requirements}
 {brackets_close}'''
 
-def nbt_helper(obj):
-    nbt = ""
-    if "nbt" in obj:
-        nbt = str(obj["nbt"]).replace("'","\\\"")
-    if "nbt_str" in obj:
-        nbt = obj["nbt_str"]
-    return nbt
+def access_helper(obj, location):
+    try:
+        for i in location:
+            if i in obj:
+                obj = obj[i]
+            else:
+                print("Error!!!")
+                print("While reading {} from {} in obj: {}".format(i, str(location), str(obj)))
+                exit(2)
+        return obj
+    except:
+        print("An Error Occoured!")
+        exit(3)
 
-def task_helper(obj):
-    return obj["task"]
 
-def feature_helper(obj):
-    return obj["features"]
+def id_helper(parent_id :str, child_id :str) -> str:
+    return parent_id + "/" + child_id
+
+def task_helper(obj) -> str:
+    return access_helper(obj, ["task"])
 
 def combine(name :str, identifier :str, desc :str, task :str, features, icon :str, nbt :str = "", parent :str = "", background :str = ""):
     global default_out
@@ -122,19 +121,7 @@ def combine(name :str, identifier :str, desc :str, task :str, features, icon :st
     if nbt != "":
         nbt = '"nbt": "{}",'.format(nbt)
 
-    frame = "task"
-    if "display" in features:
-        frame = features["display"]
-    show_toast = "true"
-    if "pop_up" in features:
-        toast = str(features["pop_up"]).lower()
-    chat = "true"
-    if "chat" in features:
-        chat = str(features["chat"]).lower()
-    hidden = "false"
-    if "hidden" in features:
-        hidden = str(features["hidden"]).lower()
-
+    [frame, toast, chat, hidden] = features
     print(task)
     criteria_requirements = ""
 
@@ -157,31 +144,167 @@ def combine(name :str, identifier :str, desc :str, task :str, features, icon :st
         )
     )
 
+
+class Remapper():
+    remapping_category_to_child = {
+        "category": "name",
+        "id": "id",
+        "description": "description",
+        "task": "task",
+        "features": "features",
+        "icon": "icon",
+        "nbt": "nbt",
+        "nbt_str": "nbt_str",
+        "background": "background"
+    }
+    def remap_category(category):
+        out_obj = {}
+        for i in Remapper.remapping_category_to_child.keys():
+            if i in category:
+                out_obj[Remapper.remapping_category_to_child[i]] = category[i]
+        return out_obj
+
+    def remap_child_id(child, category):
+        if "id" in child and "id" in category:
+            child["id"] = category["id"] + "/" + child["id"]
+        if "parent" in child and "id" in category:
+            child["parent"] = category["id"] + "/" + child["parent"]
+        return child
+
+    remapping_features_to_child = {
+        "display": "frame",
+        "pop_up": "show_toast",
+		"chat": "announce_to_chat",
+        "hidden": "hidden"
+    }
+    def remap_features(obj):
+        features = {} 
+        if "features" in obj:
+            features = obj.pop("features")
+        for i in Remapper.remapping_features_to_child.keys():
+            if i in features:
+                obj[Remapper.remapping_features_to_child[i]] = features[i]
+        return obj
+
+    def remap_nbt(obj):
+        if "nbt" in obj:
+            obj["nbt"] = str(obj["nbt"]).replace("'", "\\\"")
+        if "nbt_str" in obj:
+            obj["nbt"] = obj.pop("nbt_str")
+        return obj
+
+    remapping_names = {
+        "background": ["display", "background"],
+        "icon": ["display", "icon", "item"],
+        "nbt": ["display", "icon", "nbt"],
+        "name": ["display", "title"],
+        "description": ["display", "description"],
+        "frame": ["display", "frame"],
+        "show_toast": ["display", "show_toast"],
+        "announce_to_chat": ["display", "announce_to_chat"],
+        "hidden": ["display", "hidden"],
+        "parent": ["parent"]
+    }
+    def remap_names(obj):
+        out_obj = {}
+        def recursive_write(array :list, data, level :int = 0):
+            if level < len(array):
+                return {
+                    array[level]: recursive_write(array, data, level + 1)
+                }
+            else:
+                return data
+
+        def deepupdate(a, b):
+            for i in b.keys():
+                if i not in a:
+                    a[i] = b[i]
+                elif isinstance(a[i], dict):
+                    deepupdate(a[i], b[i]) 
+            return a
+
+        for i in obj.keys():
+            if i in Remapper.remapping_names:
+                out_obj = deepupdate(
+                        out_obj,
+                        recursive_write(Remapper.remapping_names[i], obj[i])
+                )
+            else:
+                out_obj[i] = obj[i]
+        return out_obj
+    
+    def remap_id_to_file(obj):
+        filename = ""
+        if "id" in obj:
+            filename = Mcid(obj.pop("id")).getFolder()
+        return (filename, obj)
+
+with open("config.yaml") as file:
+    try:
+        data = yaml.safe_load(file)
+        file.close()
+    except:
+        print("yaml.safe_load crashed!!!!")
+        file.close()
+        exit(1)
+
 entries = []
-
 for category in data:
-    entries.append(combine(
-        category["category"],
-        category["id"] + "/root",
-        category["description"],
-        task_helper(category),
-        feature_helper(category),
-        category["icon"],
-        nbt = nbt_helper(category),
-        background = category["background"]
-    ))
+    entries.append(
+        Remapper.remap_category(category)
+    )
+    if "children" in category:
+        for child in category["children"]:
+            entries.append(Remapper.remap_child_id(child, category))
 
-    for child in category["children"]:
+for i in range(len(entries)):
+    entries[i] = Remapper.remap_features(entries[i])
+    entries[i] = Remapper.remap_nbt(entries[i])
+    entries[i] = Remapper.remap_names(entries[i])
+    entries[i] = Remapper.remap_id_to_file(entries[i])
+print(json.dumps(entries, indent=4))
+
+   
+default_out = '''{
+    {parent}
+    "display": {
+		"show_toast": {show_toast},
+		"announce_to_chat": {announce_to_chat},
+		"hidden": {hidden}
+    },
+    {criteria_requirements}
+}'''
+
+
+
+
+'''
+try:
+    for category in data:
         entries.append(combine(
-            child["name"],
-            child["id"],
-            child["description"],
-            task_helper(child),
-            feature_helper(child),
-            child["icon"],
-            nbt = nbt_helper(child),
-            parent = child["parent"]
+            access_helper(category, ["category"]),
+            access_helper(category, ["id"]) + "/root",
+            access_helper(category, ["description"]),
+            task_helper(category),
+            feature_helper(category),
+            access_helper(category, ["icon"]),
+            nbt = nbt_helper(category),
+            background = access_helper(category, ["background"])
         ))
 
-print(entries)
-
+        for child in access_helper(category, ["children"]):
+            entries.append(combine(
+                access_helper(child, ["name"]),
+                id_helper(access_helper(category, ["id"]), access_helper(child, ["id"])),
+                access_helper(child, ["description"]),
+                task_helper(child),
+                feature_helper(child),
+                access_helper(child, ["icon"]),
+                nbt = nbt_helper(child),
+                parent = id_helper(access_helper(category, ["id"]), access_helper(child, ["parent"]))
+            ))
+except Exception as e:
+    print("something went wrong!!!")
+    print(e)
+'''
+#print(json.dumps(entries, indent=4))
